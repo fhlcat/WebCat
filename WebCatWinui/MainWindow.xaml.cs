58e332733f2e5e64aee9a9ba91b4afe5cc2394db
fhlca <fhlcat@outlook.com>
2025-08-17 14:08:17 +0800

    finish base model

--- WebCatWinui/MainWindow.xaml.cs
+++ WebCatWinui/MainWindow.xaml.cs
@@ -10,8 +10,10 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using WebCat;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -24,10 +26,31 @@ namespace WebCatWinui
     public sealed partial class MainWindow : Window
     {
         private readonly MainViewmodel _viewModel = new();
-        
+
         public MainWindow()
         {
+            _viewModel.WorkFinishedAsync += ShowResultAsync;
             InitializeComponent();
+        }
+
+        private async Task ShowResultAsync(Main.MainResult record)
+        {
+            var results = record.Value.Select(pair =>
+                pair.Key.Title +
+                ':' +
+                string.Join(", ", pair.Value)
+            );
+            var content = record.Query + string.Join('\n', results);
+            var dialog = new ContentDialog
+            {
+                XamlRoot = Content.XamlRoot,
+                Title = "Save your work?",
+                CloseButtonText = "OK",
+                DefaultButton = ContentDialogButton.Close,
+                Content = content
+            };
+
+            await dialog.ShowAsync();
         }
     }
 }
--- WebCatWinui/MainWindowViewmodel.cs
+++ WebCatWinui/MainWindowViewmodel.cs
@@ -1,4 +1,11 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.FSharp.Control;
+using Microsoft.FSharp.Core;
+using WebCat;
+using static WebCat.Main;
 
 namespace WebCatWinui;
 
@@ -17,4 +24,70 @@ public partial class MainViewmodel : ObservableObject
     [ObservableProperty] public partial double Interval { get; set; } = 1000;
 
     [ObservableProperty] public partial bool IsHeadless { get; set; } = true;
+
+    [ObservableProperty] public partial double FetchingProgress { get; private set; } = 0;
+
+    [ObservableProperty] public partial double ProcessingProgress { get; private set; } = 0;
+
+    private Process.ProcessOptions ProcessOptions => new(
+        ApiKey!,
+        (float)Temperature,
+        Model,
+        Endpoint
+    );
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(CanExecuteQuery))]
+    public partial bool IsRunning { get; private set; }
+
+    private bool CanExecuteQuery => !IsRunning;
+    
+    public event Func<MainResult, Task>? WorkFinishedAsync;
+
+    [RelayCommand(CanExecute = nameof(IsRunning))]
+    public async Task WorkAsync()
+    {
+        IsRunning = true;
+
+        var onFetchStart = FSharpOption<FSharpFunc<Main.Progress<Bing.SearchEngineResult>, Unit>>.Some(
+            FuncConvert.FromAction((Main.Progress<Bing.SearchEngineResult> progress) =>
+                {
+                    if (progress.Current == 0)
+                    {
+                        return;
+                    }
+
+                    FetchingProgress = (double)progress.Current / progress.Total * 100;
+                }
+            )
+        );
+        var onProcessStart = FSharpOption<FSharpFunc<Main.Progress<BrowserUtils.Webpage>, Unit>>.Some(
+            FuncConvert.FromAction((Main.Progress<BrowserUtils.Webpage> progress) =>
+                {
+                    if (progress.Current == 0)
+                    {
+                        return;
+                    }
+
+                    FetchingProgress = (double)progress.Current / progress.Total * 100;
+                }
+            )
+        );
+        var options = new MainOptions(
+            (int)Interval,
+            BrowserUtils.Browser.Chrome,
+            IsHeadless,
+            ProcessOptions,
+            onFetchStart,
+            onProcessStart
+        );
+        var result = await FSharpAsync.StartAsTask(runMainAsync(Query, options), null, null);
+        var finishingTask = WorkFinishedAsync?.Invoke(result);
+        if (finishingTask is not null)
+        {
+            await finishingTask;
+        }
+        
+        IsRunning = false;
+    }
 }
